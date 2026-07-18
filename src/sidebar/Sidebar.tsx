b13472b5f2f3f06cd2012be03cb5c82d98997265
pearlor <pearlor@yahoo.com>
2026-07-17 22:54:52 -0700

    skeleton

---
+++ src/sidebar/Sidebar.tsx
@@ -0,0 +1,23 @@
+export default function Sidebar() {
+  /*
+        
+    */
+  return (
+    <div className="sidebar">
+      <h2>Recipe helper</h2>
+      <section>
+        <button>Your fridge</button>
+        <br />
+        <button>New conversation</button>
+      </section>
+      <section>
+        <h3>Recent creations</h3>
+        <ul>
+          <li>Peanut butter and jelly sandwich</li>
+          <li>Grilled cheese sandwich</li>
+          <li>Spaghetti with marinara sauce</li>
+        </ul>
+      </section>
+    </div>
+  );
+}
